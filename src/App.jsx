e3240ef67224a714f08a459f7b31c089eeab1809
chems eddine bourabia <bourabiachemseddine@gmail.COM>
2024-07-16 18:20:58 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+   <div className='App bg-black min-w-full h-screen'>
+    <h1 className='text-3xl font-bold underline text-white text-center pt-10'>To Do List</h1>
+   </div>
+  )
+}
+
+export default App
